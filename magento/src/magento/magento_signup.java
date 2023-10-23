0257d2311aa71f26cad504d98cfce51b1ceb9e7c
NADEENAWWAD <Nadeenawwad78@gmail.com>
2023-10-23 12:38:22 +0300

    update

---
+++ magento/src/magento/magento_signup.java
@@ -0,0 +1,28 @@
+package magento;
+
+public class magento_signup {
+
+	
+	@beforetest 
+    public void setup() {
+
+		
+	}
+
+
+
+@test
+public void printItemName() {
+
+
+
+}
+@aftertest
+public void myPostTesting() {
+
+	
+}
+
+
+
+}
